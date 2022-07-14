0a5ea73b3be216f00aa53bf99d7d660a5ba598cb
Ankit Roy <ankit.roy@greenwayappliances.com>
2022-07-14 09:32:15 +0530

    c

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\Development\sdk\Flutter\flutter_1.22.6-stable"
+export "FLUTTER_APPLICATION_PATH=D:\Development\flutter\indiap2p_agent_mobile\packages\webview_flutter\webview_flutter_wkwebview"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=D:\Development\sdk\Flutter\flutter_1.22.6-stable\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=2.5.0"
+export "FLUTTER_BUILD_NUMBER=2.5.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
